ff2f91b60317b44ccf13cdf1a3d446c27cb33e4d
Sreerag <sreeragsanilkumar@gmail.com>
2023-12-02 10:13:43 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,32 +1,34 @@
 from libgen_api import LibgenSearch
 import logging
+import json
 
 
 class Records:
-    def __init__(self, query: str) -> None:
+
+    def __init__(self, query: str, record_pool) -> None:
         """
         Initialize the variables
         """
-
         self.query = query
         self.libgen = LibgenSearch()
-        self.record = []
+        self.records = record_pool
         self.formatted_records = []
         self.download_links = []
         self.reply_text = ""
         self.pointer = 0
 
-    def initialize_records(self) -> None:
+    def initialize_records(self, uid: str) -> None:
         """
         Initialize records
         Creates a list of dictionaries of the query results
         """
 
-        self.record = self.libgen.search_title(self.query)
-        logging.info(f"{self.initialize_records.__name__} | Records initialized")
-        logging.info(self.record)
+        self.records[str(uid)] = self.libgen.search_title(self.query)
+        logging.info(
+            f"{self.initialize_records.__name__} | Records initialized")
+        logging.info(json.dumps(self.records, indent=2))
 
-    def get_records(self, flag: int) -> [str, list]:
+    def get_records(self, flag: int, uid: str) -> [str, list]:
         """
         Get initial records when flag == 0
         Get next record when flag == 1
@@ -34,22 +36,23 @@ class Records:
         """
 
         if flag == 0:
-            self.reply_text = self.get_formatted_text(0)
+            self.reply_text = self.get_formatted_text(0, uid)
             self.download_links = self.libgen.resolve_download_links(
-                self.record[0])
+                self.records[uid][0])
             return self.reply_text, self.download_links
 
         elif flag == -1:
             self.pointer -= 1
 
             if self.pointer > 0:
-                self.reply_text = self.get_formatted_text(1)
+                self.reply_text = self.get_formatted_text(1, uid)
                 self.download_links = self.libgen.resolve_download_links(
-                    self.record[self.pointer])
+                    self.records[uid][self.pointer])
                 return self.reply_text, self.download_links
 
             else:
-                logging.info(f"{self.get_records.__name__} | No more previous records.")
+                logging.info(
+                    f"{self.get_records.__name__} | No more previous records.")
                 self.reply_text = "No more previous records!"
                 self.download_links = None
                 return self.reply_text, self.download_links
@@ -57,10 +60,10 @@ class Records:
         elif flag == 1:
             self.pointer += 1
 
-            if self.pointer < len(self.record):
-                self.reply_text = self.get_formatted_text(1)
+            if self.pointer < len(self.records[uid]):
+                self.reply_text = self.get_formatted_text(1, uid)
                 self.download_links = self.libgen.resolve_download_links(
-                    self.record[self.pointer])
+                    self.records[uid][self.pointer])
                 return self.reply_text, self.download_links
 
             else:
@@ -71,7 +74,7 @@ class Records:
         else:
             logging.warning("Invalid flag!")
 
-    def get_formatted_text(self, flag: int) -> str: 
+    def get_formatted_text(self, flag: int, uid: str) -> str:
         """
         Get the formatted text 
         """
@@ -79,34 +82,36 @@ class Records:
         formatted_text = ""
         if flag == 0:
             formatted_text = f"""
-Title: {self.record[0]['Title']}\n
-Author : {self.record[0]['Author']}\n
-Year: {self.record[0]['Year']}\n
-Extension: {self.record[0]['Extension']}\n
+Title: {self.records[uid][0]['Title']}\n
+Author : {self.records[uid][0]['Author']}\n
+Year: {self.records[uid][0]['Year']}\n
+Extension: {self.records[uid][0]['Extension']}\n
 Direct Download Links:\n
 """
             return formatted_text
         else:
             formatted_text = f"""
-Title: {self.record[self.pointer]['Title']}\n
-Author : {self.record[self.pointer]['Author']}\n
-Year: {self.record[self.pointer]['Year']}\n
-Extension: {self.record[self.pointer]['Extension']}\n
+Title: {self.records[uid][self.pointer]['Title']}\n
+Author : {self.records[uid][self.pointer]['Author']}\n
+Year: {self.records[uid][self.pointer]['Year']}\n
+Extension: {self.records[uid][self.pointer]['Extension']}\n
 Direct Download Links:\n
 """
             return formatted_text
 
-    def reset(self) -> None:
+    def reset(self, uid: str) -> None:
         """
         Resets the pointer and clears records and download links
         """
 
         self.pointer = 0
-        logging.info(f"{self.reset.__name__} | Pointer reset! | {self.pointer}")
+        logging.info(
+            f"{self.reset.__name__} | Pointer reset! | {self.pointer}")
 
-        self.record.clear()
-        logging.info(f"{self.reset.__name__} | Records cleared! | {self.record}")
+        self.records[uid] = []
+        logging.info(
+            f"{self.reset.__name__} | Records cleared! | {self.records[uid]}")
 
         self.download_links.clear()
-        logging.info(f"{self.reset.__name__} | Download links cleared! | {self.download_links}")
-
+        logging.info(
+            f"{self.reset.__name__} | Download links cleared! | {self.download_links}")
